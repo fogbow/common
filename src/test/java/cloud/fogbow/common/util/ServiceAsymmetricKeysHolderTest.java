e1f0cee0974d374e1604b01227eea47097896459
Anderson Dantas <a.kleber.d@gmail.com>
2019-02-11 16:15:40 -0300

    Implemented tests for ServiceAsymmetricKeysHolder

---
+++ src/test/java/cloud/fogbow/common/util/ServiceAsymmetricKeysHolderTest.java
@@ -0,0 +1,57 @@
+package cloud.fogbow.common.util;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.Key;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+import static org.junit.Assert.*;
+
+public class ServiceAsymmetricKeysHolderTest {
+
+    private ServiceAsymmetricKeysHolder keysHolder;
+    private String pubKeyPath;
+    private String privKeyPath;
+
+    @Before
+    public void setUp() throws Exception {
+        this.keysHolder = ServiceAsymmetricKeysHolder.getInstance();
+
+        String keysPath = HomeDir.getPath();
+        pubKeyPath = keysPath + "public.key";
+        privKeyPath = keysPath + "private.key";
+
+        this.keysHolder.setPublicKeyFilePath(pubKeyPath);
+        this.keysHolder.setPrivateKeyFilePath(privKeyPath);
+    }
+
+    @Test
+    public void getPublicKey() throws IOException, GeneralSecurityException {
+        //setup
+        RSAPublicKey expectedPublicKey = RSAUtil.getPublicKey(pubKeyPath);
+
+        // exercise
+        RSAPublicKey publicKey = this.keysHolder.getPublicKey();
+
+        // verify
+        assertEquals(expectedPublicKey, publicKey);
+    }
+
+    @Test
+    public void getPrivateKey() throws IOException, GeneralSecurityException {
+        //setup
+        RSAPrivateKey expectedPrivateKey = RSAUtil.getPrivateKey(privKeyPath);
+
+        // exercise
+        RSAPrivateKey privateKey = this.keysHolder.getPrivateKey();
+
+        // verify
+        assertEquals(expectedPrivateKey, privateKey);
+    }
+}
