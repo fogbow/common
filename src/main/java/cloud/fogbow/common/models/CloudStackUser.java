21868f2f9c9d5b934e8927cad16a374fff0655e4
Paulo Ewerton <pauloewerton@lsd.ufcg.edu.br>
2019-04-01 16:05:08 -0300

    Adding domain name to CloudStackUser

--- src/main/java/cloud/fogbow/common/models/CloudStackUser.java
+++ src/main/java/cloud/fogbow/common/models/CloudStackUser.java
@@ -3,11 +3,17 @@ package cloud.fogbow.common.models;
 import java.util.HashMap;
 
 public class CloudStackUser extends CloudUser {
+    private String domain;
     private HashMap<String, String> cookieHeaders;
 
-    public CloudStackUser(String userId, String userName, String tokenValue, HashMap<String, String> cookieHeaders) {
+    public CloudStackUser(String userId, String userName, String tokenValue, String domain, HashMap<String, String> cookieHeaders) {
         super(userId, userName, tokenValue);
+        this.domain = domain;
         this.cookieHeaders = cookieHeaders;
+    }
+
+    public String getDomain() {
+        return domain;
     }
 
     public HashMap<String, String> getCookieHeaders() {
