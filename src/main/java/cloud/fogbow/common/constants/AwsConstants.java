52d169b59564369efd6431e8f6cfb2d58f0146c1
raonismaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2019-06-10 11:45:08 -0300

    Creating Ipv4AddressValidator and improving aws consts

--- src/main/java/cloud/fogbow/common/constants/AwsConstants.java
+++ src/main/java/cloud/fogbow/common/constants/AwsConstants.java
@@ -4,4 +4,5 @@ public class AwsConstants {
     public static final String ACCESS_KEY = "access_key";
     public static final String SECRET_ACCESS_KEY = "secret_access_key";
     public static final String TOKEN_VALUE_SEPARATOR = ":";
+    public static final String SECURITY_RULE_ID_SEPARATOR = "#";
 }
