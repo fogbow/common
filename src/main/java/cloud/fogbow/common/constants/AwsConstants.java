a4997d81027eb8c541c5c9cfb8ea8b0cb2f7cd69
raonismaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2019-06-12 15:55:55 -0300

    adding more constants

--- src/main/java/cloud/fogbow/common/constants/AwsConstants.java
+++ src/main/java/cloud/fogbow/common/constants/AwsConstants.java
@@ -4,5 +4,9 @@ public class AwsConstants {
     public static final String ACCESS_KEY = "access_key";
     public static final String SECRET_ACCESS_KEY = "secret_access_key";
     public static final String TOKEN_VALUE_SEPARATOR = ":";
-    public static final String SECURITY_RULE_ID_SEPARATOR = "#";
+    public static final String SECURITY_RULE_ID_SEPARATOR = "@";
+    public static final String CPU_QUOTA = "cpu_quota";
+    public static final String RAM_QUOTA = "ram_quota";
+    public static final String INSTANCES_QUOTA = "instances_quota";
+
 }
