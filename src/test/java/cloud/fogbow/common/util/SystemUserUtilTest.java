913c8e83806fb66262a70bdd7050d4515cbb3c53
Anderson Dantas <a.kleber.d@gmail.com>
2019-07-08 17:40:36 -0300

    Implemented more tests for SystemUserUtil

--- src/test/java/cloud/fogbow/common/util/SystemUserUtilTest.java
+++ src/test/java/cloud/fogbow/common/util/SystemUserUtilTest.java
@@ -6,7 +6,12 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class SystemUserUtilTest {
-    // Test if a SystemUser is correctly seralized and later deserialized
+
+    public static final String FAKE_USER_ID = "fakeUserId";
+    public static final String FAKE_USER_NAME = "fakeUserName";
+    public static final String FAKE_IDENTITY_PROVIDER = "fakeProviderId";
+
+    // Test if a SystemUser is correctly serialized and later deserialized
     @Test
     public void testSerializationDeserealizationOfSystemUserObject() throws UnexpectedException {
         // Setup
@@ -20,7 +25,30 @@ public class SystemUserUtilTest {
         Assert.assertEquals(systemUser, recoveredSystemUser);
     }
 
+    // Test if size of content is going to overflow the limit a SystemUSer is allowed
+    @Test(expected = UnexpectedException.class)
+    public void testCreateUserWithTooMuchData() throws UnexpectedException {
+
+        // setup
+        String FAKE_USER_NAME = getLongUserName();
+
+        SystemUser systemUser = new SystemUser(FAKE_USER_ID, FAKE_USER_NAME, FAKE_IDENTITY_PROVIDER);
+
+        // exercise/verify
+        SystemUserUtil.serialize(systemUser);
+    }
+
+    private String getLongUserName() {
+        String userName = "";
+
+        for (int i = 0; i < 2 * SystemUserUtil.SERIALIZED_SYSTEM_USER_MAX_SIZE; ++i) {
+            userName += "A";
+        }
+
+        return userName;
+    }
+
     private SystemUser createSystemUser() {
-        return new SystemUser("fakeUserId", "fakeUserName", "fakeProviderId");
+        return new SystemUser(FAKE_USER_ID, FAKE_USER_NAME, "fakeProviderId");
     }
 }
