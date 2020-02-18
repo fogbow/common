1891eda11d3aa32e640f801eb164e96dda67b2ae
wassoares <wlfsoares@gmail.com>
2020-02-18 14:19:44 -0300

    Added new constants into AzureConstants class

--- src/main/java/cloud/fogbow/common/constants/AzureConstants.java
+++ src/main/java/cloud/fogbow/common/constants/AzureConstants.java
@@ -2,6 +2,11 @@ package cloud.fogbow.common.constants;
 
 public class AzureConstants {
 
-    public static final String TOKEN_VALUE_SEPARATOR = ":";
+    public static final String ACCESS_KEY = "cloud_user_credentials_access_key";
+    public static final String CLIENT_KEY = "cloud_user_credentials_client_key";
+    public static final String SUBSCRIPTION_KEY = "cloud_user_credentials_subscription_key";
+    public static final String TENANT_KEY = "cloud_user_credentials_tenant_key";
+    public static final String TOKEN_SEPARATOR = ":";
+    public static final String URL_SEPARATOR = "/";
     
 }
