0b76b4855b55ab7e35e230873b574c44a27cf228
Jadson Luan <jadson.silva@ccc.ufcg.edu.br>
2020-02-20 17:06:49 -0300

    Create a credentials enum into AzureConstants

--- src/main/java/cloud/fogbow/common/constants/AzureConstants.java
+++ src/main/java/cloud/fogbow/common/constants/AzureConstants.java
@@ -2,11 +2,23 @@ package cloud.fogbow.common.constants;
 
 public class AzureConstants {
 
-    public static final String CLIENT_ID_KEY = "client_id";
-    public static final String TENANT_ID_KEY = "tenant_id";
-    public static final String CLIENT_KEY = "client_key";
-    public static final String SUBSCRIPTION_ID_KEY = "subscription_id";
-    public static final String RESOURCE_GROUP_NAME_KEY = "resource_group_name";
-    public static final String REGION_NAME_KEY = "region_name";
+    public enum Credential {
+        CLIENT_ID_KEY("client_id"),
+        TENANT_ID_KEY("tenant_id"),
+        CLIENT_KEY("client_key"),
+        SUBSCRIPTION_ID_KEY("subscription_id"),
+        RESOURCE_GROUP_NAME_KEY("resource_group_name"),
+        REGION_NAME_KEY("region_name");
+
+        private String value;
+
+        Credential(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return this.value;
+        }
+    }
 
 }
