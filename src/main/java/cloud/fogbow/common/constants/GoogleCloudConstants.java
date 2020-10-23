1d9c4bea05bdd375945467d9924ce3316a6739c2
Pedro Silva <pedro.henrique.silva@ccc.ufcg.edu.br>
2020-10-23 12:48:23 -0300

    Added Google Cloud constants and Google Cloud user support to project id

--- src/main/java/cloud/fogbow/common/constants/GoogleCloudConstants.java
+++ src/main/java/cloud/fogbow/common/constants/GoogleCloudConstants.java
@@ -3,4 +3,21 @@ package cloud.fogbow.common.constants;
 public class GoogleCloudConstants {
     public static final String AUTHORIZATION_KEY = "Authorization";
     public static final String BEARER_S = "Bearer %s";
+    public static final String ENDPOINT_SEPARATOR = "/";
+
+    public static class Network {
+        public static final String FIREWALL_ENDPOINT = "/global/firewalls";
+        public static final String FIREWALL_CIDR_INCOME_JSON = "sourceRanges";
+        public static final String FIREWALL_CIDR_OUTCOME_JSON = "destinationRanges";
+        public static final String FIREWALL_ALLOWED_JSON = "allowed";
+        public static final String FIREWALL_ALLOWED_PORT_JSON = "port";
+        public static final String FIREWALL_ALLOWED_IP_PROTOCOL_JSON = "IPProtocol";
+        public static final String FIREWALL_CONNECTION_DIRECTION_JSON = "direction";
+        public static final String FIREWALL_SECURITY_RULES_JSON = "items";
+        public static final String ID_KEY_JSON = "id";
+        public static final String FIREWALL_NAME_JSON = "name";
+        public static final String FIREWALL_ID_JSON = "id";
+        public static final String FIREWALL_NETWORK_JSON = "id";
+        public static final String FIREWALL_ETHER_TYPE_JSON = "ethertype";
+    }
 }
--- src/main/java/cloud/fogbow/common/models/GoogleCloudUser.java
+++ src/main/java/cloud/fogbow/common/models/GoogleCloudUser.java
@@ -1,7 +1,22 @@
 package cloud.fogbow.common.models;
 
 public class GoogleCloudUser extends CloudUser {
+
+    private String projectId;
+
     public GoogleCloudUser(String userId, String userName, String token) {
         super(userId, userName, token);
+    }
+    public GoogleCloudUser(String userId, String userName, String token, String projectId) {
+        super(userId, userName, token);
+        this.projectId = projectId;
+    }
+
+    public String getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(String projectId) {
+        this.projectId = projectId;
     }
 }
