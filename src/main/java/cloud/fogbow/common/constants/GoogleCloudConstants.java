b63e3431565807c6551db367791efdd45dc814e1
Pedro Silva <pedro.henrique.silva@ccc.ufcg.edu.br>
2020-10-23 21:05:44 -0300

    Refactored the Google Cloud Constants.
    The constants shall be repeated only if they represent two different resources or entities with the same value or if the code cohesion would be compromised with their absence.

--- src/main/java/cloud/fogbow/common/constants/GoogleCloudConstants.java
+++ src/main/java/cloud/fogbow/common/constants/GoogleCloudConstants.java
@@ -2,38 +2,31 @@ package cloud.fogbow.common.constants;
 
 public class GoogleCloudConstants {
 
-
+    // GENERAL USAGE
     public static final String AUTHORIZATION_KEY = "Authorization";
     public static final String BEARER_S = "Bearer %s";
-    public static final String COMPUTE_V1_API_ENDPOINT = "/v1";
     public static final String ENDPOINT_SEPARATOR = "/";
-    public static final String VOLUME_ENDPOINT = "/volume";
-    public static final String COMPUTE_ENDPOINT = "/compute";
-    public static final String ZONES_KEY_ENDPOINT = "/zones";
-    public static final String INSTANCES_KEY_ENDPOINT = "/instances";
-    public static final String ATTACH_DISK_KEY_ENDPOINT = "/attachDisk";
-    public static final String DETACH_DISK_KEY_ENDPOINT = "/DetachDisk";
     public static final String PREDEFINED_ZONE = "";
     public static final String DEVICE_NAME_QUERY_PARAM = "?deviceName=";
     public static final String DEFAULT_ZONE = "southamerica-east1-b";
-    public static final String DISKS_KEY_ENDPOINT = "/disks";
-    public static final String PROJECTS_KEY_ENDPOINT = "/projects";
-    public static final String PATH_PROJECT = "/projects";
-    public static final String PATH_ZONE = "/zones";
-    public static final String LINE_SEPARATOR = "/";
-    public static final String V1_API_ENDPOINT = "/v1";
+    public static final String ZONE_KEY_CONFIG = "zone";
+
+    // GENERAL ENDPOINTS
+    public static final String ATTACH_DISK_KEY_ENDPOINT = "/attachDisk";
+    public static final String DETACH_DISK_KEY_ENDPOINT = "/DetachDisk";
+    public static final String PROJECT_ENDPOINT = "/projects";
+    public static final String ZONES_ENDPOINT = "/zones";
+    public static final String VOLUME_ENDPOINT = "/volume";
+    public static final String DISKS_ENDPOINT = "/disks";
+    public static final String INSTANCES_ENDPOINT = "/instances";
     public static final String GLOBAL_IP_ENDPOINT = "/global/addresses";
     public static final String GLOBAL_IMAGES_ENDPOINT = "/global/images";
     public static final String GLOBAL_NETWORKS_ENDPOINT = "/global/networks";
-    public static final String COMPUTE_ENGINE_ENDPOINT = "/compute";
+    public static final String GLOBAL_FIREWALL_ENDPOINT = "/global/firewalls";
     public static final String COMPUTE_ENGINE_V1_ENDPOINT = "/v1";
     public static final String FLAVOR_ENDPOINT = "/machineTypes";
-    public static final String COMPUTE_ATT_ENDPOINT = "/instances";
-    public static final String ZONE_KEY_CONFIG = "zone";
-    public static final String STATUS_KEY_JSON = "status";
-    public static final String GLOBAL_IP_NETWORK = "/global/networks";
-    public static final String REGION_ENDPOINT = "/regions";
-    public static final String SUBNET_ENDPOINT = "/subnetworks";
+    public static final String REGIONS_ENDPOINT = "/regions";
+    public static final String SUBNETS_ENDPOINT = "/subnetworks";
 
     public static class Volume {
         public static final String VOLUME_KEY_JSON = "volume";
@@ -56,6 +49,8 @@ public class GoogleCloudConstants {
     }
 
     public static class Network{
+        public static final String DEFAULT_NETWORK_NAME = "default";
+        
         public static final String NETWORK_KEY_JSON = "network";
         public static final String NAME_KEY_JSON = "name";
         public static final String ID_KEY_JSON = "id";
@@ -63,23 +58,25 @@ public class GoogleCloudConstants {
         public static final String CIDR_KEY_JSON = "ipCidrRange";
         public static final String ROUTING_MODE_KEY_JSON = "routingMode";
         public static final String ROUTING_CONFIG_KEY_JSON = "routingConfig";
-        public static final String DEFAULT_NETWORK_KEY = "default";
         public static final String TARGET_LINK_KEY_JSON = "targetLink";
-        public static final String FIREWALL_ENDPOINT = "/global/firewalls";
-        public static final String FIREWALL_CIDR_INCOME_JSON = "sourceRanges";
-        public static final String FIREWALL_CIDR_OUTCOME_JSON = "destinationRanges";
-        public static final String FIREWALL_ALLOWED_JSON = "allowed";
-        public static final String FIREWALL_ALLOWED_PORT_JSON = "port";
-        public static final String FIREWALL_ALLOWED_IP_PROTOCOL_JSON = "IPProtocol";
-        public static final String FIREWALL_CONNECTION_DIRECTION_JSON = "direction";
-        public static final String FIREWALL_SECURITY_RULES_JSON = "items";
-        public static final String FIREWALL_NAME_JSON = "name";
-        public static final String FIREWALL_ID_JSON = "id";
-        public static final String FIREWALL_NETWORK_JSON = "id";
-        public static final String FIREWALL_ETHER_TYPE_JSON = "ethertype";
+
+        public static class Firewall {
+            public static final String NAME_KEY_JSON = "name";
+            public static final String ID_KEY_JSON = "id";
+            public static final String CIDR_INCOME_JSON = "sourceRanges";
+            public static final String CIDR_OUTCOME_KEY_JSON = "destinationRanges";
+            public static final String ALLOWED_KEY_JSON = "allowed";
+            public static final String PORT_KEY_JSON = "port";
+            public static final String IP_PROTOCOL_KEY_JSON = "IPProtocol";
+            public static final String DIRECTION_KEY_JSON = "direction";
+            public static final String ETHERTYPE_KEY_JSON = "ethertype";
+            public static final String FIREWALLS_JSON = "items";
+        }
     }
 
     public static final class Compute {
+        public static final String CUSTOM_FLAVOR = "custom";
+
         public static final String TARGET_ID_KEY_JSON = "targetId";
         public static final String ID_KEY_JSON = "id";
         public static final String NAME_KEY_JSON = "name";
@@ -89,14 +86,14 @@ public class GoogleCloudConstants {
         public static final String METADATA_KEY_JSON = "metadata";
         public static final String PUBLIC_SSH_KEY_JSON = "ssh-keys";
         public static final String USER_DATA_KEY_JSON = "startup-script";
-        public static final String CUSTOM_FLAVOR_KEY = "custom";
         public static final String FAULT_MSG_KEY_JSON = "error.message";
         public static final String ADDRESS_KEY_JSON = "networkIP";
+        public static final String STATUS_KEY_JSON = "status";
 
         public static final class Disk {
-            public static final String INITIAL_PARAMS_KEY_JSON = "initializeParams";
-            public static final String BOOT_KEY_JSON = "boot";
             public static final boolean BOOT_DEFAULT_VALUE = true;
+
+            public static final String INITIAL_PARAMS_KEY_JSON = "initializeParams";
 
             public static final class InitializeParams {
                 public static final String IMAGE_KEY_JSON = "sourceImage";
