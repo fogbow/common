2f8c44ec558245f47e4e7dad7a1a2dea4e7d7f99
Armstrong Goes <armstrongmsg@gmail.com>
2020-12-29 22:22:49 -0300

    Added some XML utils methods

---
+++ src/main/java/cloud/fogbow/common/util/XMLUtils.java
@@ -0,0 +1,34 @@
+package cloud.fogbow.common.util;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.xml.sax.InputSource;
+
+import cloud.fogbow.common.exceptions.ConfigurationErrorException;
+
+public class XMLUtils {
+    
+    public static Element getRootNodeFromXMLString(String xmlString) throws ConfigurationErrorException {
+        SAXBuilder builder = new SAXBuilder();
+        Document document = buildDocument(xmlString, builder);
+        Element rootNode = document.getRootElement();
+        return rootNode;
+    }
+    
+    private static Document buildDocument(String xmlString, SAXBuilder builder) throws ConfigurationErrorException {
+        Document document = null;
+        try {
+            InputSource is = new InputSource(new StringReader(xmlString));
+            document = builder.build(is);
+        } catch (JDOMException | IOException e) {
+            // TODO add message
+            throw new ConfigurationErrorException();
+        }
+        return document;
+    }
+}
