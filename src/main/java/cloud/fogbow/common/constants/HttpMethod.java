31100d47c62cc4d64d65f6916f96ac84dcb664c4
Armstrong Goes <armstrongmsg@gmail.com>
2022-05-20 12:34:27 -0300

    Added missing http method

--- src/main/java/cloud/fogbow/common/constants/HttpMethod.java
+++ src/main/java/cloud/fogbow/common/constants/HttpMethod.java
@@ -1,7 +1,7 @@
 package cloud.fogbow.common.constants;
 
 public enum HttpMethod {
-    GET("GET"), POST("POST"), HEAD("HEAD"), OPTIONS("OPTIONS"), PUT("PUT"), DELETE("DELETE"), TRACE("TRACE");
+    GET("GET"), POST("POST"), HEAD("HEAD"), OPTIONS("OPTIONS"), PUT("PUT"), DELETE("DELETE"), TRACE("TRACE"), PATCH("PATCH");
 
     private String name;
 
